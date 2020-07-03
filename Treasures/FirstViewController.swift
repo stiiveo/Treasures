b1f0ffa43dc40ace13149780754cd697a1e51dee
jasonOu <1@2.com>
2020-07-03 17:17:21 +0800

    Initial Commit

---
+++ Treasures/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Treasures
+//
+//  Created by Jason Ou Yang on 2020/7/3.
+//  Copyright © 2020 Jason Ou Yang. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Treasures/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Treasures
+//
+//  Created by Jason Ou Yang on 2020/7/3.
+//  Copyright © 2020 Jason Ou Yang. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
