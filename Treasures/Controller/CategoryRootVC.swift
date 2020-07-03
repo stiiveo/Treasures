ac98cf43120463024e0d58bf1cc222b8f5e5d9a5
jasonOu <1@2.com>
2020-07-03 20:01:40 +0800

    Create each storyboard's respective viewController files.

---
+++ Treasures/Controller/CategoryRootVC.swift
@@ -0,0 +1,30 @@
+//
+//  CategoryRootVC.swift
+//  Treasures
+//
+//  Created by Jason Ou Yang on 2020/7/3.
+//  Copyright © 2020 Jason Ou Yang. All rights reserved.
+//
+
+import UIKit
+
+class CategoryRootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
