6e91d4ddbbe7d5c3eab12c69f999b6c687a8a51a
jasonOu <1@2.com>
2020-07-07 10:20:41 +0800

    Delete default hint message generated by Xcode in TreasureRootVC's func viewdidLoad().

--- Treasures/Controller/TreasureRootVC.swift
+++ Treasures/Controller/TreasureRootVC.swift
@@ -12,11 +12,7 @@ class TreasureRootVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    
 
 }
